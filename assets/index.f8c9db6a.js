42c569cc39c87e10e0b9d67069fad61a52e71b77
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-01-03 10:00:29 +0000

    Deploying to gh-pages from @ musiccajs/website@883d8931291491d33a3758860798e2775bd93943 🚀

---
+++ assets/index.f8c9db6a.js
@@ -0,0 +1,1 @@
+import{S as e}from"./Spinner.06630ae5.js";import{d as o,o as r,q as n}from"./vendor.e47133c2.js";import"./index.26f3a4e9.js";const m=o({setup(s){return(t,a)=>(r(),n(e))}});export{m as default};
